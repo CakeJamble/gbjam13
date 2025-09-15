a5d0806b1108b60fe2baf801030136a29465f7cf
cakejamble <campbell.jakelog@gmail.com>
2025-09-15 13:22:16 -0700

    Bugfix for lazy set volume

--- class/SoundManager.lua
+++ class/SoundManager.lua
@@ -8,6 +8,7 @@ local SoundManager = Class{}
 function SoundManager:init(sounds)
 	self.sounds = sounds or {}
 	self.volume = 1.0
+	self.activeSounds = {}
 end;
 
 ---@param key string
@@ -20,13 +21,26 @@ function SoundManager:play(key)
 	local base = variants[i]
 
 	local sound = base:clone()
+	sound:setVolume(self.volume)
 	sound:play()
 
+	table.insert(self.activeSounds, sound)
 	return sound
 end;
 
 function SoundManager:setVolume(v)
 	self.volume = v
+	for _,src in ipairs(self.activeSounds) do
+		src:setVolume(self.volume)
+	end
+end;
+
+function SoundManager:update(dt)
+	for i,src in ipairs(self.activeSounds) do
+		if not src:isPlaying() then
+			table.remove(self.activeSounds, i)
+		end
+	end
 end;
 
 return SoundManager
